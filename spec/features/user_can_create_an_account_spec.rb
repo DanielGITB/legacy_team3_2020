b6d2a71dde6abc6a622d71d82115b38427aa8968
dernathul <lautaroparracab@gmail.com>
2020-02-13 19:20:56 +0100

    sign up happy path tested

--- spec/features/user_can_create_an_account_spec.rb
+++ spec/features/user_can_create_an_account_spec.rb
@@ -1,39 +1,29 @@
 require 'rails_helper'
-require 'factory_bot'
-require 'factory_bot_rails'
 
 
-#describe 'Factory' do
-#  it 'should have valid Factory' do
-#      expect(FactoryBot.create(:user)).to be_valid
-#  end
-#end
+
 describe 'account home page' do
-
-  it 'has a sign up button' do
+  before do
     visit root_path
-    expect(page).to have_content 'Sign up'
   end
 
+
+
+  it 'create an account' do
+    click_on 'Sign up'
+    expect(page).to have_content 'characters'
+    binding.pry
+  end
+
+  it 'can fill account details' do
+  fill_in 'Name', with: 'John'
+  fill_in 'Email', with: 'john@sit.com'
+  fill_in 'Password', with: 'qwert12345'
+  fill_in 'Password confirmation', with: 'qwert12345'
+  click_on 'Create'
+  end
 
 end
-
-describe 'sign up page' do
-  before do
-    visit new_user_registration_path
-  end
-
-
-
-  it 'can access the sign up page' do
-    expect(page).to have_content 'characters'
-  end
-
-  it 'can find username text fill' do
-  expect(@name).to eq nil
-  end
 
   
 
-
-end
