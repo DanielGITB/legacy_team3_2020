4569a4d6ec01c3a261f2a7d176ca6861c724945a
DanielB <1996.daniel.bryant@gmail.com>
2020-02-13 15:03:33 +0100

    creating basic tests for visiting specific webpages. and added global variablesfor the text fill in fields

--- spec/features/user_can_create_an_account_spec.rb
+++ spec/features/user_can_create_an_account_spec.rb
@@ -1,52 +1,39 @@
 require 'rails_helper'
+require 'factory_bot'
+require 'factory_bot_rails'
 
-# describe Account do
-#   let (:account) { instance_double(name: 'John',email: 'pinochio@real.boy',Password: 'gepetto',Password_Confirmation: 'gepetto')}
+
+#describe 'Factory' do
+#  it 'should have valid Factory' do
+#      expect(FactoryBot.create(:user)).to be_valid
+#  end
+#end
+describe 'account home page' do
+
+  it 'has a sign up button' do
+    visit root_path
+    expect(page).to have_content 'Sign up'
+  end
 
 
-#   before do
-#     visit root_path
-#   end
-#   click_on('Sign up')
+end
+
+describe 'sign up page' do
+  before do
+    visit new_user_registration_path
+  end
+
+
+
+  it 'can access the sign up page' do
+    expect(page).to have_content 'characters'
+  end
+
+  it 'can find username text fill' do
+  expect(@name).to eq nil
+  end
+
   
-   
-#   it 'displays Sign up form' do
-#     expect(page).to have_content 'Sign up'
-# end
-feature 'display the sign up page' do
-    context 'with sign up form' do
-      before do
-      visit new_user_registration_path
-      end
-    end
 
 
-  it 'display Name' do
-    expect(page)to have_content (text_field)
-  end
-
-  it 'display name Email' do
-   expect(page)to have_content (text_field)
-  end
-
-  it 'display name Password' do
-    expect(page)to have_content (text_field)
-  end
-
-  it 'display name Password confirmation' do
-    expect(page)to have_content (text_field)
-  end
-
-  it 'display Create button' do (sad path)
-    if name = nil 'require name'
-      if email = nil 'require email'
-        if Password = less than  8 characters 'password must contain at least 8 characters'
-          if Password_Confirmation = not match password 'password must match'
-    expect(click_on).to Create_Account = 'check information'
-
-     it 'display Create button' do
-      expect(click_on). to Create_Account = 'Welcome! You have signed up successfully.'
-
-  end
-  
 end
