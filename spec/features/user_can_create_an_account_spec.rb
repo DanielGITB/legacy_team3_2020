6d34ddc8097e3179bd787d054e2ece6dd3a1ae71
dernathul <lautaroparracab@gmail.com>
2020-02-14 11:17:25 +0100

    signup test passed (happy path)

--- spec/features/user_can_create_an_account_spec.rb
+++ spec/features/user_can_create_an_account_spec.rb
@@ -2,27 +2,26 @@ require 'rails_helper'
 
 
 
-describe 'account home page' do
-  before do
-    visit root_path
+describe 'user can sign up' do
+  describe 'succesfully' do
+    before do
+      visit root_path
+      click_on 'Sign up'
+      fill_in 'Name', with: 'John'
+      fill_in 'Email', with: 'john@sit.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Password confirmation', with: 'password'
+      click_on 'Create'
+    end
+
+    it 'redirect to root_path' do
+      expect(current_path).to eq root_path  
+    end
+  
+    it 'display welcome message' do
+      expect(page).to have_content 'Welcome! You have signed up successfully.'
+    end
   end
-
-
-
-  it 'create an account' do
-    click_on 'Sign up'
-    expect(page).to have_content 'characters'
-    binding.pry
-  end
-
-  it 'can fill account details' do
-  fill_in 'Name', with: 'John'
-  fill_in 'Email', with: 'john@sit.com'
-  fill_in 'Password', with: 'qwert12345'
-  fill_in 'Password confirmation', with: 'qwert12345'
-  click_on 'Create'
-  end
-
 end
 
   
